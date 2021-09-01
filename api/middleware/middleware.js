846981d093d848a1feec3ba416b9b71068908967
cn8817 <christinenguyen1433@gmail.com>
2021-09-01 14:49:33 -0700

    completed middleware and now debugging routers

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,13 +1,40 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.originalUrl}`
+  )
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({
+        message:'user not found'
+      })
+    } else{
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message:'problem finding user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()){
+    res.status(400).json({
+      message:'missing required name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
@@ -15,3 +42,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
